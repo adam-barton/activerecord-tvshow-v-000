b404e7132a992e09041a36b827cff89ae5d8268b
Adam Barton <adam.barton333@gmail.com>
2018-10-02 04:05:02 +0000

    automatically backed up by learn

--- app/models/shows.rb
+++ app/models/shows.rb
@@ -8,4 +8,12 @@ class Show < ActiveRecord::Base
     Show.where(highest_rating).first
   end
 
+  def self.lowest_rating
+    Show.minimum(:rating)
+  end
+
+  def self.least_popular_show
+    Show.where(lowest_rating).first
+  end
+
 end
